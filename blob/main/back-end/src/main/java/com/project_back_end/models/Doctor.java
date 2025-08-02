ec2524cd6cb2e82a8328b500a1e898a92f7dba68
Saurav Kumar Singh <sauravkumarsingh9991@gmail.com>
2025-08-02 22:08:54 +0530

    Doctor.java

--- blob/main/back-end/src/main/java/com/project_back_end/models/Doctor.java
+++ blob/main/back-end/src/main/java/com/project_back_end/models/Doctor.java
@@ -1,1 +1,24 @@
 
+package com.project_back_end.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    private String specialization;
+
+    @ElementCollection
+    private List<String> availableTimes; // Example: ["Monday 10-12", "Wednesday 2-4"]
+
+    // Getters and setters
+}
